ccd95dadfc71a384882902c691762b1039732784
Natalia <averegina@male.ru>
2022-12-09 20:47:20 +0300

    решена задача 3

---
+++ homework5/3/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+float[] numbers = new float[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Искомый массив: ");
+PrintArray(numbers);
+
+float min = Int32.MaxValue;
+float max = Int32.MinValue;
+for (int x = 0; x < numbers.Length; x++)
+{
+    if (numbers[x] > max)
+    {
+        max = numbers[x];
+    }
+    if (numbers[x] < min)
+    {
+        min = numbers[x];
+    }
+}
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+
+Console.WriteLine($"Дельта между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(float[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = (new Random().Next(100,1000)) / 100;
+    }
+}
+void PrintArray(float[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
